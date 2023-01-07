c6f795d33af4e59eb19a30a69b7710d9c105f9eb
florentin72 <florentin.paillier@gmail.com>
2023-01-07 15:37:15 +0100

    test duplication

--- src/main/java/fr/ensim/info/a/Zoo/ZooApplication.java
+++ src/main/java/fr/ensim/info/a/Zoo/ZooApplication.java
@@ -2,6 +2,7 @@ package fr.ensim.info.a.Zoo;
 
 import fr.ensim.info.a.Zoo.model.Secteur;
 import fr.ensim.info.a.Zoo.model.Tigre;
+import fr.ensim.info.a.Zoo.model.Zebre;
 import fr.ensim.info.a.Zoo.model.Zoo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,10 +22,29 @@ public class ZooApplication {
 		zoo = new Zoo();
 		zoo.setMaxVisiteurParSecteur(15);
 		Secteur<Tigre> secteurTigre = new Secteur<>();
+		Secteur<Zebre> secteurZebre= new Secteur<>();
 		secteurTigre.setAnimalList(new ArrayList<>());
+		secteurZebre.setAnimalList(new ArrayList<>());
 		for (int i=0; i< 50; i++){
 			secteurTigre.getAnimalList().add(new Tigre());
 		}
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+		secteurZebre.getAnimalList().add(new Zebre());
+
 		zoo.setSecteurList(new Stack<>());
 		zoo.getSecteurList().add(secteurTigre);
 	}
